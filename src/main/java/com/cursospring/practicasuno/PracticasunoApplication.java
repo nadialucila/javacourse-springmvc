09a6e3084666f15d9134a9190a1569209095589f
nadialucila <nadia.bergara17@gmail.com>
2025-02-05 20:59:36 -0300

    first commit

---
+++ src/main/java/com/cursospring/practicasuno/PracticasunoApplication.java
@@ -0,0 +1,13 @@
+package com.cursospring.practicasuno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticasunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticasunoApplication.class, args);
+	}
+
+}
